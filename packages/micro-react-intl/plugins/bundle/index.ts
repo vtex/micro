b71da4a6c8f1ecbe5916c578e398d498c3a92dd2
Tiago Gimenes <tlgimenes@gmail.com>
2020-06-02 12:03:25 -0300

    adding babel config

--- packages/micro-react-intl/plugins/bundle/index.ts
+++ packages/micro-react-intl/plugins/bundle/index.ts
@@ -4,7 +4,8 @@ import {
   pagesRuntimeName
 } from '@vtex/micro-core'
 import { cacheGroup } from '@vtex/micro-react/plugins'
-import { Block, Context, group, resolve } from 'webpack-blocks'
+import { join } from 'path'
+import { babel, Block, Context, group, resolve } from 'webpack-blocks'
 
 import { aliases } from '../aliases'
 
@@ -12,6 +13,18 @@ export default class Bundle extends BundlePlugin {
   public getWebpackConfig = async (config: Block<Context>): Promise<Block<Context>> => {
     return group([
       config,
+      babel({
+        plugins: [
+          [
+            require.resolve('babel-plugin-react-intl'), {
+              outputEmptyJson: false,
+              extractFromFormatMessageCall: true,
+              moduleSourceName: '@vtex/micro-react-intl/components',
+              messagesDir: join(this.project.dist, this.target, 'messages-per-file')
+            }
+          ] as any
+        ]
+      }),
       resolve({
         alias: aliases.reduce(
           (acc, packageName) => {
---
+++ packages/micro-react-intl/plugins/bundle/reactIntlPlugin.ts
@@ -0,0 +1,1 @@
+// TODO: Write plugin in here

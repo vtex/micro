7169b2e00b2988e65b5de0c578e6c14d3f96ddbf
Tiago Gimenes <tlgimenes@gmail.com>
2020-06-04 12:33:08 -0300

    enforce webpack next in yarn resolutions

--- examples/simple/plugins/bundle/index.ts
+++ examples/simple/plugins/bundle/index.ts
@@ -1,14 +1,14 @@
 import { BundlePlugin } from '@vtex/micro-core/lib'
 import { htmlTags, purgeCSS } from '@vtex/micro-css/plugins'
-import { Block, Context, group } from 'webpack-blocks'
+import { Block, group } from 'webpack-blocks'
 
 export default class Bundle extends BundlePlugin {
-  public getWebpackConfig = async (config: Block<Context>): Promise<Block<Context>> => {
+  public getWebpackConfig = async (config: Block): Promise<Block> => {
     return group([
       config,
       purgeCSS({
-        whitelist: () => htmlTags
-      })
+        whitelist: () => htmlTags,
+      }),
     ])
   }
 }
--- packages/micro-core/plugins/bundle/index.ts
+++ packages/micro-core/plugins/bundle/index.ts
@@ -1,4 +1,20 @@
-import { Block, group } from 'webpack-blocks'
+import { join } from 'path'
+
+import TimeFixPlugin from 'time-fix-plugin'
+import { Configuration } from 'webpack'
+import {
+  addPlugins,
+  Block,
+  customConfig,
+  defineConstants,
+  env,
+  group,
+  optimization,
+  resolve,
+  setContext,
+  setMode,
+  sourceMaps,
+} from 'webpack-blocks'
 
 import { BundlePlugin } from '../../lib/lifecycles/bundle'
 
@@ -7,8 +23,50 @@ export default class Bundle extends BundlePlugin {
     config: Block
     // target: BundleTarget
   ): Promise<Block> => {
-    const block: Block[] = []
+    const wpInternalsDist = join(
+      this.project.dist,
+      this.target,
+      'webpack_internals'
+    )
+    const cacheConfig: Configuration = {
+      cache: {
+        name: this.mode,
+        version: this.mode,
+        type: 'filesystem',
+        managedPaths: ['./node_modules'].map((p) =>
+          join(this.project.rootPath, p)
+        ),
+        cacheDirectory: join(wpInternalsDist, 'cache'),
+      },
+      recordsPath: join(wpInternalsDist, 'records.json'),
+    }
+    const block: Array<Block | Configuration> = [
+      customConfig(cacheConfig),
+      setMode(this.mode),
+      setContext(this.project.rootPath),
+      defineConstants({
+        'process.env.NODE_ENV': this.mode,
+      }),
+      optimization({
+        runtimeChunk: {
+          name: 'webpack-runtime',
+        },
+      } as any), // TODO: why this as any ?
+      env('development', [
+        addPlugins([new TimeFixPlugin()]),
+        sourceMaps('inline-source-map'),
+        customConfig({
+          watchOptions: {
+            ignored: `${this.project.dist}`,
+            aggregateTimeout: 300,
+          },
+        }) as Block,
+      ]),
+      resolve({
+        extensions: ['.tsx', '.ts', '.js', '.jsx', '.json'],
+      }),
+    ]
 
-    return group([config, ...block])
+    return group([config, ...(block as any)])
   }
 }

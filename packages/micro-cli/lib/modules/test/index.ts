18acb1ed126410f7cc7165ba6ee8cb55435e2555
victorhmp <victor2142@gmail.com>
2020-06-07 18:30:43 -0300

    Improve test main method docs
    Add disclaimer comment explaining the reason for accessing process args
    directly instead of using the formatted ones provided by 'findhelp'.

--- packages/micro-cli/lib/modules/test/index.ts
+++ packages/micro-cli/lib/modules/test/index.ts
@@ -1,6 +1,12 @@
 import { startTests } from './script'
 
 const main = () => {
+  /**
+   * Using process.argv here instead of the object used when
+   * main() is called because it enables us to pass these args
+   * as they are to Jest. That way we don't need to replicate
+   * Jest's API.
+   */
   const args = process.argv.slice(2)
 
   startTests(...args.slice(1))

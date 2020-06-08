03ee5453fa299364e4b69838aac6ce3d3b9f4389
victorhmp <victor2142@gmail.com>
2020-06-07 23:01:16 -0300

    Use babel-preset-react-app instead of @babel/preset-react

--- packages/micro-cli/lib/modules/test/babelTransform.ts
+++ packages/micro-cli/lib/modules/test/babelTransform.ts
@@ -3,7 +3,7 @@ import babelJest from 'babel-jest'
 module.exports = babelJest.createTransformer({
   presets: [
     require.resolve('@babel/preset-env'),
-    require.resolve('@babel/preset-react'),
+    require.resolve('babel-preset-react-app'),
     require.resolve('@babel/preset-typescript'),
   ],
   plugins: [
